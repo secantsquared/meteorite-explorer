f3afb2a718cc4db0fe51c28943fe3c0646f67e7d
Ryan Boris <ryan.boris@gmail.com>
2021-02-21 19:08:57 -0500

    add redis to staging deploy

--- backend/server/index.js
+++ backend/server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
-// const redis = require('redis')
+const redis = require('redis')
 //server
 
 const PORT = process.env.PORT || 5000
@@ -10,31 +10,31 @@ const server = express()
 server.use(cors())
 server.use(express.json())
 
-// const PORT_REDIS = process.env.PORT || 6379
-// const redisClient = redis.createClient(PORT_REDIS)
+const PORT_REDIS = process.env.PORT || 6379
+const redisClient = redis.createClient(PORT_REDIS)
 
-// const set = (key, value) => redisClient.set(key, JSON.stringify(value))
+const set = (key, value) => redisClient.set(key, JSON.stringify(value))
 
-// const get = (req, res, next) => {
-//   const key = req.query.page
-//   redisClient.get(key, (error, data) => {
-//     if (error) {
-//       res.status(400).send(error)
-//     }
-//     if (data !== null) {
-//       res.status(200).send(JSON.parse(data))
-//     } else {
-//       next()
-//     }
-//   })
-// }
+const get = (req, res, next) => {
+  const key = req.query.page
+  redisClient.get(key, (error, data) => {
+    if (error) {
+      res.status(400).send(error)
+    }
+    if (data !== null) {
+      res.status(200).send(JSON.parse(data))
+    } else {
+      next()
+    }
+  })
+}
 
-server.get('/api/jsonData/', async (req, res) => {
+server.get('/api/jsonData/', get, async (req, res) => {
   try {
     const { data } = await axios.get(
       `${process.env.METEORITE_URL}?$offset=${req.query.page}&$limit=20`
     )
-    // set(req.query.page, data)
+    set(req.query.page, data)
     return res.status(200).json(data)
   } catch (err) {
     return res.status(500).json({ msg: err.message, code: err.code })
